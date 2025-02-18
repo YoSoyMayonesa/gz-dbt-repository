9bc75fc73f0a178553e1391bbbeb22e05de34b08
marjoriegomezb@gmail.com <marjoriegomezb@gmail.com>
2025-02-18 14:54:26 +0000

    change columns and delete duplicates

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,46 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost,
+        ship_cost
+
+    from source
+
+)
+
+select * from renamed;
+
+with 
+
+source as (
+
+    select * from {{ source('raw', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        cast (ship_cost as int64) as ship_cost,
+        
+
+    from source
+
+)
+
+select * from renamed
+
--- models/tables.sql
+++ models/tables.sql
@@ -10,5 +10,31 @@ FROM {{source('raw', 'sales')}};
 SELECT 
     products_id as product_id,
     purchSE_PRICE as purchase_price,
-FROM {{source('raw', 'product')}}
+FROM {{source('raw', 'product')}};
 
+SELECT 
+    *,
+FROM {{source('raw', 'ship')}};
+
+with
+
+ source as (
+
+     select * from {{ source('raw', 'sales') }}
+
+ ),
+
+ renamed as (
+
+     select
+         date_date,
+         orders_id,
+         pdt_id,
+         revenue,
+         quantity
+
+     from source
+
+ )
+
+ select * from renamed
